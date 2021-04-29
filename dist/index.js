b7d5d399ab0ec907232d08224b6802c211de4532
Kitzunu <24550914+Kitzunu@users.noreply.github.com>
2021-04-29 16:06:17 +0200

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -3005,7 +3005,7 @@ class IssueLabeler {
                 }
             }
             if (!found)
-                yield this.SetLabel(issue, 'Invalid-IncompleteData/OrNotTrinityCore');
+                yield this.SetLabel(issue, 'Invalid-MissingHash/Commit');
             core.debug('SetMissingHashLabel end');
         });
     }
